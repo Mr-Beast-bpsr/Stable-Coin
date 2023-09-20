f64575c6e8b9dfda5c311cf398c3c49bf7ea71e4
BhanuFromBlockCoder <bhanu@theblockcoders.com>
2023-09-20 12:16:07 +0530

    token-contract

---
+++ src/DSCEngine.sol
@@ -0,0 +1,42 @@
+ // SPDX-License-Identifier: MIT 
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+ pragma solidity ^0.8.18; 
+
+
+
+
+ contract DSCEngine {
+
+    /*
+ * @title DecentralizedStableCoin
+ * @author Mr-Beast-Bpsr
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+ * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
+ */
+ constructor() {  }
+
+
+} 
---
+++ src/StableCoin.sol
@@ -0,0 +1,65 @@
+ // SPDX-License-Identifier: MIT 
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+pragma solidity ^0.8.18; 
+import {ERC20Burnable,ERC20}  from "@openzepplin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzepplin/contracts/access/Ownable.sol"; 
+ contract StableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    /*
+    /*
+ * @title DecentralizedStableCoin
+ * @author Mr-Beast-Bpsr
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+ * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
+ */
+ 
+ constructor()ERC20("StableBeastCoin","SBC") {  }
+
+  function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+} 
